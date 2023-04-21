f249a7b59929274b9af6a78215abb1dc6224bda7
WILLROCK0505 <131421747+WILLROCK0505@users.noreply.github.com>
2023-04-21 14:45:36 +0800

    Update basic.vue

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -9,24 +9,24 @@
 <script lang="ts" setup>
 const tableData = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: '',
+    name: '',
+    address: '',
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: '',
+    name: '',
+    address: '',
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: '',
+    name: '',
+    address: '',
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: '',
+    name: '',
+    address: '',
   },
 ]
 </script>
